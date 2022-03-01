eb118a9a4d7c34741cfde4075d47c3566fb3990e
Marzan1810662 <hassanmarzan17@gmail.com>
2022-03-01 15:09:11 +0600

    all results shown with responsiveness

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 const searchProducts = () => {
-    const searchText = document.getElementById('search-field').value.toLowerCase();
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value.toLowerCase();
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    /*  clearing search field after clicking search button */
+    searchField.value = '';
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => displaySearchResult(data.data))
-    .catch(error => console.log(error));
+        .then(response => response.json())
+        .then(data => displaySearchResult(data.data))
+        .catch(error => console.log(error));
 }
 
-const displaySearchResult = (products) =>{
-    console.log(products);
-    for(const product of products){
-        console.log(product);
-    }
+const displaySearchResult = (products) => {
+    // console.log(products);
+    const searchReasults = document.getElementById('search-result-container');
+    products.forEach(product => {
+        console.log(product.brand, product.image, product.phone_name);
+        const div = document.createElement('div');
+        div.classList.add('col', 'p-3');
+        div.innerHTML = `
+                    <div class="card h-auto text-center search-result-card">
+                        <img src="${product.image}" class="card-img-top w-75 mx-auto p-3" alt="phone">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.brand}</h5>
+                            <p class="card-text">${product.phone_name}</p>
+                            <a  href="#">
+                            <button class="btn border border-2 w-75">Details</button>
+                            </a>
+                        </div>
+                    </div>
+        `;
+        searchReasults.appendChild(div);
+    });
 }
 
 
