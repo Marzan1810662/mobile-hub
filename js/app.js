b37ea8156c9f062cf36f5a5ffb1e7eb671b9b534
Marzan1810662 <hassanmarzan17@gmail.com>
2022-03-01 13:17:05 +0600

    products successfully fetched with api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('app.js connected');
+const searchProducts = () => {
+    const searchText = document.getElementById('search-field').value.toLowerCase();
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displaySearchResult(data.data))
+    .catch(error => console.log(error));
+}
+
+const displaySearchResult = (products) =>{
+    console.log(products);
+    for(const product of products){
+        console.log(product);
+    }
+}
+
+
