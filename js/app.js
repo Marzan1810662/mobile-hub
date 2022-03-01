debc3ec0bcd182539e4045b64d1305c843885878
Marzan1810662 <hassanmarzan17@gmail.com>
2022-03-01 23:25:08 +0600

    functionalities added to show more button

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const searchProducts = () => {
             .catch(error => displayErrorMessage('Something went wrong. Please try again later'));
     }
 }
-let allProducts;
+
 /* display spinner */
 const displaySpinner = () => {
     document.getElementById('spinner-div').style.display = 'block';
@@ -26,8 +26,6 @@ const displaySpinner = () => {
 /* display search results */
 const displaySearchResult = (products) => {
     const searchReasults = document.getElementById('search-result-container');
-    allProducts = products;
-    console.log(allProducts);
 
     /* clearing previous search results */
     document.getElementById('error-div').style.display = 'none';
@@ -60,6 +58,7 @@ const displaySearchResult = (products) => {
     document.getElementById('spinner-div').style.display = 'none';
 }
 
+/* create card to display searched products */
 const showProduct = (product) => {
     const div = document.createElement('div');
     div.classList.add('col', 'p-3');
@@ -77,9 +76,11 @@ const showProduct = (product) => {
 `;
     return div;
 }
-
+let allproducts;
 /* show more button to show all the products */
 const displayShowAllButton = (products) => {
+    console.log(products);
+    allproducts = products;
     const div = document.createElement('div');
     div.classList.add('text-center');
     div.innerHTML = `
@@ -91,7 +92,8 @@ const displayShowAllButton = (products) => {
 /* show all products when 'show all' is clicked */
 
 const showAllProducts = () => {
-     console.log(allproducts);
+console.log(allproducts);
+    const searchReasults = document.getElementById('search-result-container');
     allproducts.forEach(product => {
         const div = showProduct(product);
         searchReasults.appendChild(div);
@@ -110,6 +112,8 @@ const loadProductDetails = (productId) => {
 /* display product details */
 const displayProductDetail = (product) => {
     const productDetailContainer = document.getElementById('product-detail-container');
+    /* remove previous product details */
+    productDetailContainer.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card', 'h-100', 'mb-3', 'py-2')
     div.innerHTML = `
@@ -127,7 +131,7 @@ const displayProductDetail = (product) => {
             </ul>
             <p class="card-text"><span class="fw-bold">- Chip:</span> ${product.mainFeatures.chipSet ? product.mainFeatures.chipSet : ''}</p>
             <p class="card-text"><span class="fw-bold">- Storage:</span> ${product.mainFeatures.storage ? product.mainFeatures.storage : ''}</p>
-            <span class="fw-bold">${product.others ? '- Others': ''}</span>
+            <span class="fw-bold">${product.others ? '- Others' : ''}</span>
             <ul id="${product.slug}others" >
             </ul>
             <p class="card-text"><small class="text-muted">${product.releaseDate ? product.releaseDate : 'No release date found'}</small></p>
@@ -143,9 +147,8 @@ const displayProductDetail = (product) => {
         document.getElementById(`${product.slug}sensors`).appendChild(li);
     });
     for (const key in product?.others) {
-        console.log(key);
         const li = document.createElement('li');
-        li.innerHTML= `<span class="fw-bold">- ${key}:</span>${product.others[key]}`;
+        li.innerHTML = `<span class="fw-bold"> ${key}:</span> ${product.others[key]}`;
         document.getElementById(`${product.slug}others`).appendChild(li);
     }
 }
